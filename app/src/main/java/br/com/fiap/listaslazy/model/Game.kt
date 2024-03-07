ea2745406176502ad2bac06ae492944c348570f6
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-03-07 13:01:43 -0300

    Lazycolumn com card.

--- app/src/main/java/br/com/fiap/listaslazy/model/Game.kt
+++ app/src/main/java/br/com/fiap/listaslazy/model/Game.kt
@@ -1,3 +1,8 @@
 package br.com.fiap.listaslazy.model
 
-data class Game()
+data class Game(
+    val id: Long = 0,
+    val title: String = "",
+    val studio: String = "",
+    val releaseYear: Int = 0,
+)
